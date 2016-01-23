4b5172b188f4248221d69406b9fbe8f62b343490
yikouniao <chinoandtippi@gmail.com>
2016-01-23 14:57:55 +0800

    Update main

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+
+static void Help() {
+  cout << "raindrop-recognition\n\n"
+       << "Waiting for update...\n"
+       << "The code is based on OpenCV3.10.\n";
+}
+
+int main(int argc, char** argv) {
+  Help();
+  return 0;
+}
