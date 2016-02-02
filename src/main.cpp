17c62c444358b3939b5b566df8da631a01f1b8b6
yikouniao <chinoandtippi@gmail.com>
2016-02-02 13:49:15 +0800

    tailor invalid area

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,22 @@ int main(int argc, char** argv) {
   help();
   Mat img = imread("images/20090423-194552-01-P.jpg");
   cvtColor(img, img, CV_BGR2GRAY);
+  img = img(Rect(0, 8, 690, img.rows - 8)); // Tailor invalid area
   namedWindow("Original image");
   imshow("Original image", img);
   img.convertTo(img, CV_32FC1);
+
+  // Anisotropic diffusion
   int k = 16, iterate = 64;
   anisotropicDiffusion(img, img, k, iterate);
+
+  // Sobel Derivatives
   sobel(img, img);
+
+  // binarization
+  double thresh = 20, maxval = 255;
+  threshold(img, img, thresh, maxval, THRESH_BINARY);
+
   img.convertTo(img, CV_8UC1);
   namedWindow("dst");
   imshow("dst", img);
--- src/sobel.cpp
+++ src/sobel.cpp
@@ -15,5 +15,7 @@ void sobel(const cv::Mat& src, cv::Mat& dst) {
   Mat abs_grad_x, abs_grad_y;
   convertScaleAbs(grad_x, abs_grad_x);
   convertScaleAbs(grad_y, abs_grad_y);
+  // Approximate the gradient by adding both directional gradients.
+  // This is not an exact calculation, but it is good for our purposes.
   addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, dst);
 }
